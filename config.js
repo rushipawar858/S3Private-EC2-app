12cd0ac2433100c898336f37fe2daa12537e1103
rushipawar858 <rushipawar5207@gmail.com>
2025-07-08 11:59:41 +0530

    Application Created

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  REGION: process.env.REGION,
+  SIGNED_URL_EXPIRY: parseInt(process.env.SIGNED_URL_EXPIRY, 10) || 60,
+  PORT: process.env.PORT || 3000,
+};
